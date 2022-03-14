17416145e020cde3417f5a51ea895ff05cff7af3
Mohammad Fatehi <94982051+Mohammad-Fatehi@users.noreply.github.com>
2022-03-15 00:25:20 +0330

    JAVA Exercise1-1

---
+++ Java Exercise1-1/src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+/*
+ * Mohammad Fatehi
+ * Java Exercise1-1
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        for(int i=4; i>=0; i--) {
+            for(int j=i; j<4; j++)
+                System.out.print("*");
+            System.out.println();
+        }
+    }
+
+}
